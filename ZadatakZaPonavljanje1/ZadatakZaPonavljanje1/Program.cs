28be7e24247d7487bdf59b088695a3e6e4b66201
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-06-06 15:58:52 +0200

    Add files via upload

---
+++ ZadatakZaPonavljanje1/ZadatakZaPonavljanje1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite neki niz znakova: ");
+            string znakovi = Console.ReadLine();
+
+            string sVelika = znakovi.ToUpper();
+            string sMala = znakovi.ToLower();
+            string sPrvaTri = znakovi.Substring(0, 3);
+            string sZadnjihPet = znakovi.Substring(znakovi.Length - 5);
+            string s8_11 = znakovi.Substring(7, 4);
+            Console.WriteLine(sVelika);
+            Console.WriteLine(sMala);
+            Console.WriteLine(sPrvaTri);
+            Console.WriteLine(sZadnjihPet);
+            Console.WriteLine(s8_11);
+            Console.ReadKey();
+        }
+    }
+}
